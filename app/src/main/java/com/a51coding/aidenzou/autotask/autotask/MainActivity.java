3ccb0e61147b419c8c9cfda727519d92802cdff5
aidenZou <aidenZou@gmail.com>
2017-07-05 23:13:46 +0800

    init

---
+++ app/src/main/java/com/a51coding/aidenzou/autotask/autotask/MainActivity.java
@@ -0,0 +1,13 @@
+package com.a51coding.aidenzou.autotask.autotask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
