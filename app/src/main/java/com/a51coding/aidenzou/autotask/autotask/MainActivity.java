ee70f998e5cc0d01e21c361c09bea3512c027d3c
aidenZou <aidenZou@gmail.com>
2017-07-06 21:29:26 +0800

    启动新的 Activity

--- app/src/main/java/com/a51coding/aidenzou/autotask/autotask/MainActivity.java
+++ app/src/main/java/com/a51coding/aidenzou/autotask/autotask/MainActivity.java
@@ -1,13 +1,31 @@
 package com.a51coding.aidenzou.autotask.autotask;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.a51coding.aidenzou.autotask.autotask.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // System.out.println("开始...");
+        // Log.d("HelloWorldActivity", "onCreate execute");
+    }
+
+    // 启动新的 Activity
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        // 启动 Intent 指定的 DisplayMessageActivity 实例
+        startActivity(intent);
     }
 }
